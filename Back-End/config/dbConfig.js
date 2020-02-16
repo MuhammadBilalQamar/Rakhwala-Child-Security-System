1936151499bf32bddcea0d4275189d777890c26f
MuhammadBilalQamar <bilalqmr1@gmail.com>
2020-02-17 00:47:53 +0500

    set up server

--- Back-End/config/dbConfig.js
+++ Back-End/config/dbConfig.js
@@ -3,7 +3,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "travelous"
+  database: "rakhwala"
 });
 
 module.exports = connection;
--- Back-End/server.js
+++ Back-End/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+var port = process.env.port || 3000;
 
-app.get('/', (req, res) => res.send('Bismillah Hirrahmanirrahim !'))
+app.get('/', (req, res) => res.send('Bismillah Hirrahmanirrahim. This  is root route !'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
